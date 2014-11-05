21e8d6775e428b3541916b5aa57702f60c94cb9c
Alex Zammitt <azammitt@gmail.com>
2014-11-05 03:52:01 +0000

    press enter to add score

--- client/templates/scores/scoreAdd.js
+++ client/templates/scores/scoreAdd.js
@@ -1,20 +1,29 @@
+var scoreAdd = function() {
+    var scoreInput =  document.getElementById("score-input");
+    var score = Number(scoreInput.value);
+    
+    if(score <= 0) {
+      swal({ type: "error", title: "Invalid Score!", text: "Score must be a positive number" });
+      return;
+    }
+    Meteor.call('scoreInsert', score, function(error, result) {
+      // display the error to the user and abort
+      if (error) {
+        return swal({ type: "error", title: error.reason });
+      } else { 
+        swal({type: "success", title: "Score added sucessfully!", text: "Nice job!" });
+        scoreInput.value = '';
+      }
+    });
+}
 
 Template.scoreAdd.events({
-  "click .add-score": function (event, template) {
-    
-    var scoreInput =  document.getElementById("score-input");
-    var score = scoreInput.value;
-	
-    if(!isNaN(score)) { 
-      Meteor.call('scoreInsert', Number(score), function(error, result) {
-        // display the error to the user and abort
-        if (error) {
-          return swal({ type: "error", title: error.reason });
-        } else { 
-          swal({type: "success", title: "Score added sucessfully!" });
-		  scoreInput.value = '';
-        }
-      });
+  "keypress #score-input": function (evt, template) {
+    if (evt.which === 13) {
+      scoreAdd();
     }
+  },
+  "click .add-score": function (event, template) {
+    scoreAdd();
   }
 });

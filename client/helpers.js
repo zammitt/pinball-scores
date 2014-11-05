fd4fa874d85198311c6f5d4905fdbf2cdef512df
Alex Zammitt <azammitt@gmail.com>
2014-11-05 02:46:01 +0000

    fixing some things

--- client/helpers.js
+++ client/helpers.js
@@ -1,9 +1,3 @@
-var DateFormats = {
-       short: "MM/DD/YYYY",
-       long: "dddd DD/MM/YYYY HH:mm"
-};
-
-UI.registerHelper("formatDate", function (datetime, format) {
-    f = DateFormats[format];
-    return moment(datetime).format(f);
+UI.registerHelper('formatDate', function(date) {
+  return moment(date).format('MM/DD/YYYY');
 });

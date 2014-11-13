91ac34eb938704e03930e2867f21a2fd2148f117
Alex Zammitt <azammitt@gmail.com>
2014-11-13 02:55:44 +0000

    profile pic for marcus

--- server/users.js
+++ server/users.js
@@ -1,6 +1,11 @@
 Meteor.methods({
   getUserAvatar: function(userId) {
     var user= Meteor.users.findOne(userId);
+    
+    if(user.profile.name == "Marcus Gilbert") {
+      return "/sunny-d-and-rum-yum-yum.jpg";
+    }
+    
     if (user && user.services && user.services.google)
     {
       return user.services.google.picture;

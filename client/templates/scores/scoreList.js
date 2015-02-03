e4b310feae066a7671856b32688ac93f7fba218c
Mark Avenius <mark@aveni.us>
2015-02-03 16:12:42 -0500

    Clearing score input early, to avoid dupes.

--- client/templates/scores/scoreList.js
+++ client/templates/scores/scoreList.js
@@ -13,15 +13,16 @@ Template.scoreList.events({
     
     var scoreInput =  document.getElementById("score-input");
     var score = scoreInput.value;
+	scoreInput.value = '';
 	
     if(!isNaN(score)) { 
       Meteor.call('scoreInsert', Number(score), function(error, result) {
         // display the error to the user and abort
         if (error) {
+		  scoreInput.value = score;
           return swal({ type: "error", title: error.reason });
         } else { 
           swal({type: "success", title: "Score added sucessfully!" });
-		  scoreInput.value = '';
         }
       });
     }
